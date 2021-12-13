40f7ce301b4f17587856662003b8da58a34bd508
yoonsangmin <dbstkdalsa@naver.com>
2021-12-13 22:56:04 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+import random
+
+# 상수
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPACE_SIZE = 50
+
+# 나중에 속도 스네이크 내로 옮기기
+SPEED = 50
+# 초기 몸 길이
+BODY_PARTS = 3
+PLAYER_COLOR =  "#00FF00"
+
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def next_turn():
+    pass
+
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+window.mainloop()
+
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
